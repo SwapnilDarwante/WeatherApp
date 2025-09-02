dacf02a457c7d7a3ea25f48a2c840f454700be37
Swapnil Darwante <swapnildarwante613@gmail.com>
2025-09-02 12:26:27 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_BASE_URL || "/WeatherApp",
   server: {
     port: 5173,
     open: true
